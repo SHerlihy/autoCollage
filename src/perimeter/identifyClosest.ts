a72cd993198164fa2ac4c18ec392f82d68d59259
SHerlihy <steven_herlihy@yahoo.com>
2022-10-28 18:47:24 +0100

    Fix identify closest

--- src/perimeter/identifyClosest.ts
+++ src/perimeter/identifyClosest.ts
@@ -4,30 +4,36 @@ import { getHypotenuseSideFromSides } from "./trigonometryHelpers";
 // regardless of direction
 export const identifyClosestCoordinateToCoordinate = (
   principleCoordinate: ICoordinates,
-  nextImgPointId: string,
-  potentialPointIds: Set<string>,
-  allOtherPoints: IPointsMap
+  potentialPoints: IPointsMap
 ) => {
-  let closestPointId = nextImgPointId;
-
   const { x: principleX, y: principleY } = principleCoordinate;
-  const { coordinates } = allOtherPoints.get(nextImgPointId)!;
+
+  const [initialPoint] = potentialPoints.values();
+
+  const { coordinates: initialCoordinates } = initialPoint;
+
+  let closestPointId = initialPoint.currentImgPointId;
 
   let closestPointDistance = getHypotenuseSideFromSides(
-    Math.abs(principleX - coordinates.x),
-    Math.abs(principleY - coordinates.y)
+    Math.abs(principleX - initialCoordinates.x),
+    Math.abs(principleY - initialCoordinates.y)
   );
 
-  for (const pointId of potentialPointIds) {
-    const { coordinates } = allOtherPoints.get(pointId)!;
+  for (const [potentialPointId, potentialPointValue] of potentialPoints) {
+    const { coordinates } = potentialPointValue;
 
     const distanceToOrigin = getHypotenuseSideFromSides(
       Math.abs(principleX - coordinates.x),
       Math.abs(principleY - coordinates.y)
     );
 
+    if (!closestPointDistance) {
+      closestPointId = potentialPointId;
+      closestPointDistance = distanceToOrigin;
+    }
+
     if (distanceToOrigin < closestPointDistance) {
-      closestPointId = pointId;
+      closestPointId = potentialPointId;
       closestPointDistance = distanceToOrigin;
     }
   }
